cd0d0217dff3abf46d78642320d2e0727c90662d
SherryH <enjoy.wind7rain@gmail.com>
2025-04-05 23:19:01 +0800

    Completed Exercise 5.1

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -21,6 +21,22 @@ function ToastProvider({ children }) {
     });
   };
 
+  // To clear the stack when Esc is pressed
+  // Not to live inside ToastShelf
+  // Clearing the stack  == clearing the data, ToastProvider handles the data
+  // ToastShelf handles the presentation, just keep it presentational
+  React.useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === 'Escape') {
+        setStack([]);
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
   return (
     <ToastContext.Provider value={{ stack, addToast, removeToastFromStack }}>
       {children}
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -6,6 +6,10 @@ import { ToastContext } from '../ToastProvider';
 
 function ToastShelf() {
   const { stack } = React.useContext(ToastContext);
+  // instead of passing and simply call removeToast(id) here
+  // we just pass id down and let Toast calling removeToast within itself
+  // we can change the <Toast removeToast /> to <Toast id />
+  // it makes more sense in terms of props API design
 
   return (
     <ol className={styles.wrapper}>
